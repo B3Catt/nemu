6e13c624d801a1e3612f207c65cf33450083418e
tracer-ics2017 <tracer@njuics.org>
2022-03-21 16:32:52 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  16:32:52 up  8:50,  1 user,  load average: 0.00, 0.00, 0.00 25b794e1f91d966a5d2045381ce2c86f5c87f981

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -136,7 +136,7 @@ static int cmd_info(char *args){
           printf("%s:\t%04x\t\n", regsw[i], reg_w(i));
         }
         for (int i = 0; i < 8; i ++){
-          printf("%s:\t%02x\t%c", regsb[i], reg_b(i), '\t' ? '\n' : (i % 2 == 0));
+          printf("%s:\t%02x\t%c", regsb[i], reg_b(i), (i % 2) ? '\n' : '\t');
         }
     }
     else if (strcmp(sub_cmd, "w") == 0)
