a81bc0da1874698f51d9e391025a15115ddbf2ff
tracer-ics2017 <tracer@njuics.org>
2022-04-22 15:30:21 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  15:30:21 up  6:37,  1 user,  load average: 0.00, 0.00, 0.00 fb18a6cf7e6a43ca42b5e3e02f9ca8bfa759f2a3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,9 +7,15 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_lr(&t0, id_dest->reg, id_dest->width);
-  rtl_subi(&t0, &t0, id_src->simm);
-  rtl_sr(id_dest->reg, id_dest->width, &t0);
+  rtl_subi(&t2, &id_dest->val, id_src->simm);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template2(sub);
 }
