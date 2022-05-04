db393591bc0ace1cd9e322e988b022ea24ecefa3
tracer-ics2017 <tracer@njuics.org>
2022-05-04 21:23:00 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:23:00 up 5 min,  1 user,  load average: 0.01, 0.02, 0.00 fafe7a0ff747af3c9a70aa0cac7c5edb1db71693

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,9 +38,10 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm = (int32_t)instr_fetch(eip, op->width);
-
-  rtl_li(&op->val, op->simm);
+  t1 = instr_fetch(eip, op->width);
+  rtl_sext(&t2, &t1, op->width);
+	op->simm = (int32_t)t2;
+	rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,7 +7,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_subi(&t2, &id_dest->val, id_src->simm);
+  rtl_subi(&t2, &id_dest->val, id_src->val);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
 
