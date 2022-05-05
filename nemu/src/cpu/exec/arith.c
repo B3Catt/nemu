8014bd33da80657ac5f3e3efce74bd0b011f442e
tracer-ics2017 <tracer@njuics.org>
2022-05-05 22:18:40 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  22:18:40 up  7:07,  1 user,  load average: 0.00, 0.00, 0.00 31ce7d318bdbe08ea85de57cc744270a99e62016

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -21,7 +21,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_subi(&t2, &id_dest->val, id_src->val);
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
   rtl_sltu(&t3, &id_dest->val, &t2);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
@@ -40,7 +40,7 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) {
-  rtl_subi(&t2, &id_dest->val, id_src->val);
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
   rtl_sltu(&t3, &id_dest->val, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
 
@@ -78,7 +78,20 @@ make_EHelper(inc) {
 }
 
 make_EHelper(dec) {
-  TODO();
+  rtl_subi(&t2, &id_dest->val, 1);
+  rtl_sltu(&t3, &id_dest->val, &t2);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_sltu(&t0, &id_dest->val, &t2);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }
