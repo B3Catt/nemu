3507730cc4ab007d783e4860f2153470bce6ff55
tracer-ics2017 <tracer@njuics.org>
2022-05-09 17:27:36 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  17:27:36 up  1:33,  1 user,  load average: 0.00, 0.13, 0.10 a88b98e8847a4572156678d02f7ebf1766c7be49

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -26,8 +26,9 @@ void cpu_exec(uint64_t n) {
   for (; n > 0; n --) {
 	/* Execute one instruction, including instruction fetch,
    * instruction decode, and the actual execution. */
-  
+#ifdef DEBUG  
   uint32_t old_eip = cpu.eip;
+#endif
 	exec_wrapper(print_flag);
 
 #ifdef DEBUG
