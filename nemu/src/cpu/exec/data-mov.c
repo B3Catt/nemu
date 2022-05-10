4c934905302d18ea0e0aba2bae2076adb36af431
tracer-ics2017 <tracer@njuics.org>
2022-05-10 21:13:41 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:13:41 up 58 min,  1 user,  load average: 0.12, 0.08, 0.19 f9036a61b24eee45f2a9d91250de6e36448f36c2

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -14,8 +14,9 @@ make_EHelper(push) {
     rtl_push(&(id_dest->val));
   }
   else if (id_dest->type == OP_TYPE_IMM) {
-    rtl_push(&id_dest->val);
-  }
+    rtl_sext(&t2, &id_dest->val, id_dest->width);
+    rtl_push(&t2);
+	}
 
   print_asm_template1(push);
 }
