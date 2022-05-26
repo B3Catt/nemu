6e49b1cc2cbf1e7559286e6c248f3296978f3490
tracer-ics2017 <tracer@njuics.org>
2022-05-26 21:26:09 +0800

    >  run 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:26:09 up 57 min,  1 user,  load average: 0.00, 0.00, 0.00 b2eefc11dba8c8799f6e42da4e9176a9bae15026

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,8 +10,7 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+  return fs_write(fd, &buf, len);
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -53,8 +52,9 @@ _RegSet* do_syscall(_RegSet *r) {
 		case SYS_none:
     SYSCALL_ARG1(r) = sys_none(); break;
 		case SYS_exit:
-		printf("&d\n", a[0]);
     SYSCALL_ARG1(r) = sys_exit(r); break;
+    case SYS_write:
+    SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
