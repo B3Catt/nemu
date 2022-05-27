47417ab8875ec6eae637686b59918db8ab4dc502
tracer-ics2017 <tracer@njuics.org>
2022-05-27 12:51:55 +0800

    >  run 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  12:51:55 up  2:07,  1 user,  load average: 0.00, 0.00, 0.00 49352b95dac0f8c37e2fd2ab74b5964be62e94

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,7 +6,14 @@ void ramdisk_read(void *buf, off_t offset, size_t len);
 
 size_t get_ramdisk_size();
 
+extern size_t fs_filesz(int fd);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern int fs_close(int fd);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
-  return (uintptr_t)DEFAULT_ENTRY;
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, as->ptr, fs_filesz(fd));
+  fs_close(fd);
+  return (uintptr_t)as->ptr;
 }
