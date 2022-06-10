34ea3b9854f18fe71412a5799b52366ed45fc978
tracer-ics2017 <tracer@njuics.org>
2022-06-10 21:53:45 +0800

    >  compile 162030131 liwenqi Linux debian 4.19.0-18-686 #1 SMP Debian 4.19.208-1 (2021-09-29) i686 GNU/Linux  21:53:45 up 1 min,  1 user,  load average: 0.04, 0.02, 0.00 3619de36547067cc6519a3c64d4cd95595e1142a

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -2,30 +2,30 @@
 #include "cpu/reg.h"
 
 paddr_t page_translate(vaddr_t vaddr, bool is_write) {
-  //Log("vaddr:0x%x", vaddr);
+  Log("vaddr:0x%x", vaddr);
  
   paddr_t base = cpu.cr3.page_directory_base << 12;
   vaddr_t dir = (vaddr >> 22) << 2;
   paddr_t pd_addr = base | dir;
-  //Log("pd_addr:0x%x", pd_addr);
+  Log("pd_addr:0x%x", pd_addr);
   PDE pd;
   pd.val = paddr_read(pd_addr, 4);
-  //Log("pd_val:0x%x", pd.val);
+  Log("pd_val:0x%x", pd.val);
   assert(pd.present);
 
   base = pd.page_frame << 12;
   dir = ((vaddr >> 12) & 0x3ff) << 2;
   paddr_t pt_addr = base | dir;
-  //Log("pt_addr:0x%x", pt_addr);
+  Log("pt_addr:0x%x", pt_addr);
   PTE pt;
   pt.val = paddr_read(pt_addr, 4);
-  //Log("pt_val:0x%x", pt.val);
+  Log("pt_val:0x%x", pt.val);
   assert(pt.present);
 
   base = pt.page_frame << 12;
   dir = vaddr & 0xfff;
   paddr_t paddr = base | dir;
-  //Log("paddr:0x%x", paddr);
+  Log("paddr:0x%x", paddr);
 
   pd.accessed = 1;
   paddr_write(pd_addr, 4, pd.val);
